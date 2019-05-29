295061f780e5457eb9de2de053a931ffd6f99ccc
vagrant <mikegithub77@gmail.com>
2019-05-29 02:13:21 +0000

    made some simple functions to transverse a tree

--- vampire.js
+++ vampire.js
@@ -1,32 +1,40 @@
 class Vampire {
-  constructor(name, yearConverted) {
-    this.name = name;
-    this.yearConverted = yearConverted;
-    this.offspring = [];
-    this.creator = null;
-  }
-
-  /** Simple tree methods **/
-
-  // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
-
-  }
-
-  // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
-
-  }
-
-  // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-
-  }
-
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
-  }
+    constructor(name, yearConverted) {
+      this.name = name;
+      this.yearConverted = yearConverted;
+      this.offspring = [];
+      this.creator = null;
+    }
+  
+    /** Simple tree methods **/
+  
+    // Adds the vampire as an offspring of this vampire
+    addOffspring(vampire) {
+      this.offspring.push(vampire);
+      vampire.creator = this;
+    }
+  
+    // Returns the total number of vampires created by that vampire
+    get numberOfOffspring() {
+      return this.offspring.length;
+    }
+  
+    // Returns the number of vampires away from the original vampire this vampire is
+    get numberOfVampiresFromOriginal() {
+      return this._numberOfVampiresFromOriginalHelper(this.creator);
+    }
+  
+      _numberOfVampiresFromOriginalHelper(currentCreator) {
+        if(currentCreator === null) {
+          return 0;
+        }
+        return 1 + this._numberOfVampiresFromOriginalHelper(currentCreator.creator);
+      }
+  
+    // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+    isMoreSeniorThan(vampire) {
+      return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal; 
+    }
 
   /** Stretch **/
 
